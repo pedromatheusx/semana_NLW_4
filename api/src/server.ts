fc6fecf09d318b34ee0cc2442fe5e6cb2e5d4584
Pedro Matheus <pedromatheusduarte@gmail.com>
2021-02-23 16:23:34 -0300

    segunda aula

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,10 @@
-import express from 'express'
+import "reflect-metadata"
+import express from "express"
+import "./database";
+import { router } from "./routes";
 
 const app = express();
+app.use(express.json());
+app.use(router);
 
-app.get("/", (req, res) => {
-    return res.json({message: "Hello World -NLW4"})
-})
-
-app.post("/", (req, res) => {
-    return res.json({message: "Hello World - NLW#4"})
-})
-
-app.listen(3333, () => console.log("Funcionando"))
+app.listen(3333, () => console.log("Funcionando"));
